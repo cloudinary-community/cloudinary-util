eac1a689b3b15233ccfe8552098d26079cc6d8e1
Colby Fayock <colbyfayock@users.noreply.github.com>
2023-02-17 22:20:35 -0500

    feat: Exporting Types (#12)
    # Description
    
    Exports all the type for the URL loader
    
    ## Issue Ticket Number
    
    Fixes #11 
    
    https://github.com/colbyfayock/next-cloudinary/issues/133
    
    <!-- Specify above which issue this fixes by referencing the issue
    number (`#<ISSUE_NUMBER>`) or issue URL. -->
    <!-- Example: Fixes
    https://github.com/colbyfayock/cloudinary-util/issues/<ISSUE_NUMBER> -->
    
    ## Type of change
    
    <!-- Please select all options that are applicable. -->
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected)
    - [ ] Fix or improve the documentation
    - [ ] This change requires a documentation update
    
    
    # Checklist
    
    <!-- These must all be followed and checked. -->
    
    - [ ] I have followed the contributing guidelines of this project as
    mentioned in [CONTRIBUTING.md](/CONTRIBUTING.md)
    - [ ] I have created an
    [issue](https://github.com/colbyfayock/cloudinary-util/issues) ticket
    for this PR
    - [ ] I have checked to ensure there aren't other open [Pull
    Requests](https://github.com/colbyfayock/cloudinary-util/pulls) for the
    same update/change?
    - [ ] I have performed a self-review of my own code
    - [ ] I have run tests locally to ensure they all pass
    - [ ] I have commented my code, particularly in hard-to-understand areas
    - [ ] I have made corresponding changes needed to the documentation

--- packages/url-loader/src/index.ts
+++ packages/url-loader/src/index.ts
@@ -1,2 +1,5 @@
 export { constructCloudinaryUrl, transformationPlugins } from './lib/cloudinary';
 export { effects, position, primary, text } from './constants/qualifiers';
+export type { ImageOptionsResize, ImageOptionsZoomPan, ImageOptions } from './types/image';
+export type { PluginSettings, PluginOverrides } from './types/plugins';
+export type { Qualifier } from './types/qualifiers';
