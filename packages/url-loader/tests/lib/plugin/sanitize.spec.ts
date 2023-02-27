1885deb7311039947095423a95665c649650de81
Colby Fayock <colbyfayock@users.noreply.github.com>
2023-02-26 21:30:43 -0500

    Update sanitize.spec.ts

--- packages/url-loader/tests/lib/plugin/sanitize.spec.ts
+++ packages/url-loader/tests/lib/plugin/sanitize.spec.ts
@@ -1,4 +1,14 @@
-import { constructCloudinaryUrl } from '../../../src/lib/cloudinary';
+import { Cloudinary } from '@cloudinary/url-gen';
+
+import * as sanitizePlugin from '../../../src/plugins/sanitize';
+
+const { plugin } = sanitizePlugin
+
+const cld = new Cloudinary({
+  cloud: {
+    cloudName: 'test-cloud-name'
+  }
+});
 
 // Mock console.warn() so we can see when it's called
 global.console = {
@@ -14,96 +24,51 @@ describe('Cloudinary Sinitize', () => {
 
   describe('constructCloudinaryUrl', () => {
     it('should include fl_sanitize when display image source name end with svg', () => {
-      const cloudName = 'customtestcloud';
-      const url = constructCloudinaryUrl({
-        options: {
-          src: 'turtle.svg'
-        },
-        config: {
-          cloud: {
-            cloudName
-          }
-        }
-      });
-
-      expect(url).toContain(
-        `https://res.cloudinary.com/${cloudName}/image/upload/fl_sanitize/f_auto/q_auto/turtle.svg`
-      );
+      const src = 'turtle.svg';
+      const cldImage = cld.image(src);
+      plugin({ cldImage, options: {
+        src
+      } });
+      expect(cldImage.toURL()).toContain(`image/upload/fl_sanitize/turtle.svg`);
     });
 
     it('should include fl_sanitize and f_svg when display format is svg', () => {
-      const cloudName = 'customtestcloud';
-      const url = constructCloudinaryUrl({
-        options: {
-          src: 'turtle',
-          format: 'svg'
-        },
-        config: {
-          cloud: {
-            cloudName
-          }
-        }
-      });
-
-      expect(url).toContain(
-        `https://res.cloudinary.com/${cloudName}/image/upload/fl_sanitize/f_svg/q_auto/turtle.svg`
-      );
+      const src = 'turtle';
+      const cldImage = cld.image(src);
+      plugin({ cldImage, options: {
+        format: 'svg',
+        src
+      } });
+      expect(cldImage.toURL()).toContain(`image/upload/fl_sanitize/turtle`);
     });
 
     it('should include fl_sanitize and f_svg when display format svg image', () => {
-      const cloudName = 'customtestcloud';
-      const url = constructCloudinaryUrl({
-        options: {
-          src: 'turtle.svg',
-          format: 'svg'
-        },
-        config: {
-          cloud: {
-            cloudName
-          }
-        }
-      });
-
-      expect(url).toContain(
-        `https://res.cloudinary.com/${cloudName}/image/upload/fl_sanitize/f_svg/q_auto/turtle.svg`
-      );
+      const src = 'turtle.svg';
+      const cldImage = cld.image(src);
+      plugin({ cldImage, options: {
+        format: 'svg',
+        src
+      } });
+      expect(cldImage.toURL()).toContain(`image/upload/fl_sanitize/turtle.svg`);
     });
 
     it('should not include fl_sanitize when set option sanitize to false', () => {
-      const cloudName = 'customtestcloud';
-      const url = constructCloudinaryUrl({
-        options: {
-          src: 'turtle.svg',
-          sanitize: false
-        },
-        config: {
-          cloud: {
-            cloudName
-          }
-        }
-      });
-
-      expect(url).toContain(
-        `https://res.cloudinary.com/${cloudName}/image/upload/f_auto/q_auto/turtle.svg`
-      );
+      const src = 'turtle.svg';
+      const cldImage = cld.image(src);
+      plugin({ cldImage, options: {
+        sanitize: false,
+        src
+      } });
+      expect(cldImage.toURL()).toContain(`image/upload/turtle.svg`);
     });
 
     it('should not include fl_sanitize when display other image', () => {
-      const cloudName = 'customtestcloud';
-      const url = constructCloudinaryUrl({
-        options: {
-          src: 'other_image_type'
-        },
-        config: {
-          cloud: {
-            cloudName
-          }
-        }
-      });
-
-      expect(url).toContain(
-        `https://res.cloudinary.com/${cloudName}/image/upload/f_auto/q_auto/notSvgImage`
-      );
+      const src = 'turtle';
+      const cldImage = cld.image(src);
+      plugin({ cldImage, options: {
+        src
+      } });
+      expect(cldImage.toURL()).toContain(`image/upload/turtle`);
     });
   });
 });
