1ef8bb468da2f5410f16d4771baa6609615cfe47
Colby Fayock <fayock@gmail.com>
2023-05-11 21:48:45 -0400

    fix: adding flags to plugin list

--- packages/url-loader/src/lib/cloudinary.ts
+++ packages/url-loader/src/lib/cloudinary.ts
@@ -3,6 +3,7 @@ import { parseUrl, ParseUrl, objectHasKey } from '@cloudinary-util/util';
 
 import * as croppingPlugin from '../plugins/cropping';
 import * as effectsPlugin from '../plugins/effects';
+import * as flagsPlugin from '../plugins/flags';
 import * as sanitizePlugin from '../plugins/sanitize';
 import * as overlaysPlugin from '../plugins/overlays';
 import * as namedTransformationsPlugin from '../plugins/named-transformations';
@@ -31,6 +32,7 @@ export const transformationPlugins = [
 
   croppingPlugin,
   effectsPlugin,
+  flagsPlugin,
   overlaysPlugin,
   sanitizePlugin,
   namedTransformationsPlugin,
