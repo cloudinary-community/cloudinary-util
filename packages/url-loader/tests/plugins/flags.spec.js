57f9a4a7f12abbaa0f893661409deda706a79288
Colby Fayock <colbyfayock@users.noreply.github.com>
2023-05-11 21:26:30 -0400

    feat: Flags (#44)
    # Description
    
    Adds the option for `flags`
    
    This isn't a comprehensive setup as there are individual use cases that
    need more finesse but the current syntax is:
    ```
    flags: ['myflag']
    # or
    flags: {
      myflag: 'value' // ex: fl_splice:transition_(name_circleopen;du_2.5)
    }
    ```
    
    https://cloudinary.com/documentation/transformation_reference#fl_flag
    
    ## Issue Ticket Number
    
    Fixes #43 
    
    <!-- Specify above which issue this fixes by referencing the issue
    number (`#<ISSUE_NUMBER>`) or issue URL. -->
    <!-- Example: Fixes
    https://github.com/colbyfayock/cloudinary-util/issues/<ISSUE_NUMBER> -->
    
    ## Type of change
    
    <!-- Please select all options that are applicable. -->
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected)
    - [ ] Fix or improve the documentation
    - [ ] This change requires a documentation update
    
    
    # Checklist
    
    <!-- These must all be followed and checked. -->
    
    - [ ] I have followed the contributing guidelines of this project as
    mentioned in [CONTRIBUTING.md](/CONTRIBUTING.md)
    - [ ] I have created an
    [issue](https://github.com/colbyfayock/cloudinary-util/issues) ticket
    for this PR
    - [ ] I have checked to ensure there aren't other open [Pull
    Requests](https://github.com/colbyfayock/cloudinary-util/pulls) for the
    same update/change?
    - [ ] I have performed a self-review of my own code
    - [ ] I have run tests locally to ensure they all pass
    - [ ] I have commented my code, particularly in hard-to-understand areas
    - [ ] I have made corresponding changes needed to the documentation

---
+++ packages/url-loader/tests/plugins/flags.spec.js
@@ -0,0 +1,58 @@
+import { Cloudinary } from '@cloudinary/url-gen';
+
+import * as flagsPlugin from '../../src/plugins/flags';
+
+const { plugin } = flagsPlugin
+
+const cld = new Cloudinary({
+  cloud: {
+    cloudName: 'test-cloud-name'
+  }
+});
+
+describe('Plugins', () => {
+  describe('Flags', () => {
+    it('should include a flag on an image', () => {
+      const src = 'turtle';
+      const assetType = 'image';
+      const cldVideo = cld.image(src);
+      const flags = ['keep_iptc'];
+      plugin({ cldAsset: cldVideo, options: {
+        assetType,
+        src,
+        flags
+      } });
+      expect(cldVideo.toURL()).toContain(`${assetType}/upload/fl_keep_iptc/${src}`);
+    }); 
+
+    it('should add multiple flags to a video', () => {
+      const src = 'turtle.mp4';
+      const assetType = 'video';
+      const cldVideo = cld.video(src);
+      const flags = ['no_stream', 'splice']
+      plugin({ cldAsset: cldVideo, options: {
+        assetType,
+        src,
+        flags
+      } });
+      expect(cldVideo.toURL()).toContain(`${assetType}/upload/fl_no_stream/fl_splice/${src}`);
+    }); 
+
+    it('should add custom flag definitions via object syntax', () => {
+      const src = 'turtle';
+      const assetType = 'image';
+      const cldVideo = cld.image(src);
+      const flags = {
+        splice: 'transition_(name_circleopen;du_2.5)',
+        attachment: 'space_jellyfish'
+      }
+      const flagsString = Object.entries(flags).map(([q, v]) => `fl_${q}:${v}`).join('/');
+      plugin({ cldAsset: cldVideo, options: {
+        assetType,
+        src,
+        flags
+      } });
+      expect(cldVideo.toURL()).toContain(`${assetType}/upload/${flagsString}/${src}`);
+    }); 
+  });
+});
