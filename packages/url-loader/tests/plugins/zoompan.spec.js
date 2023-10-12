67ecba2b9fc581add4f85ffcbfe3acec6c5027cf
Shannan Bunch <shannan_bunch@hotmail.com>
2023-10-12 12:01:17 -0700

    Adding tests for Zoompan. (#67)
    # Description
    Adding tests for Zoompan.
    <!-- Include a summary of the change made and also list the dependencies
    that are required if any -->
    
    ## Issue Ticket Number
    
    Fixes #65
    
    <!-- Specify above which issue this fixes by referencing the issue
    number (`#<ISSUE_NUMBER>`) or issue URL. -->
    <!-- Example: Fixes
    https://github.com/colbyfayock/cloudinary-util/issues/<ISSUE_NUMBER> -->
    
    ## Type of change
    <!-- Please select all options that are applicable. -->
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected)
    - [ ] Fix or improve the documentation
    - [ ] This change requires a documentation update
    
    
    # Checklist
    
    <!-- These must all be followed and checked. -->
    
    - [x] I have followed the contributing guidelines of this project as
    mentioned in [CONTRIBUTING.md](/CONTRIBUTING.md)
    - [ ] I have created an
    [issue](https://github.com/colbyfayock/cloudinary-util/issues) ticket
    for this PR
    - [x] I have checked to ensure there aren't other open [Pull
    Requests](https://github.com/colbyfayock/cloudinary-util/pulls) for the
    same update/change?
    - [x] I have performed a self-review of my own code
    - [x] I have run tests locally to ensure they all pass
    - [ ] I have commented my code, particularly in hard-to-understand areas
    - [ ] I have made corresponding changes needed to the documentation

---
+++ packages/url-loader/tests/plugins/zoompan.spec.js
@@ -0,0 +1,134 @@
+import { Cloudinary } from '@cloudinary/url-gen';
+import * as zoomPanPlugin from '../../src/plugins/zoompan';
+
+const { plugin } = zoomPanPlugin
+
+const cld = new Cloudinary({
+  cloud: {
+    cloudName: 'test-cloud-name'
+  }
+});
+
+const TEST_PUBLIC_ID = 'test-public-id';
+
+describe('Plugins', () => {
+  describe('Zoom pan', () => {
+    it('should add "e_zoompan"', () => {
+
+      const cldImage = cld.image(TEST_PUBLIC_ID);
+
+      const options = {
+        zoompan : true
+      }
+
+      const result = plugin({
+        cldAsset: cldImage,
+        options
+      });
+
+      expect(result.options.format).toBe('gif');
+      expect(cldImage.toURL()).toContain(`e_zoompan`);
+    });
+
+    it('should not zoom pan', () => {
+
+      const cldImage = cld.image(TEST_PUBLIC_ID);
+
+      plugin({
+        cldAsset: cldImage,
+        options: {}
+      });
+
+      expect(cldImage.toURL()).not.toContain(`e_zoompan`);
+    });
+
+    it('should add loop effect ', () => {
+
+      const cldImage = cld.image(TEST_PUBLIC_ID);
+
+      const options = {
+        zoompan : "loop"
+      }
+
+      plugin({
+        cldAsset: cldImage,
+        options
+      });
+
+      expect(cldImage.toURL()).toContain(`e_zoompan`);
+      expect(cldImage.toURL()).toContain(`e_loop`);
+    });
+
+    it('should add a custom zoompan', () => {
+
+      const cldImage = cld.image(TEST_PUBLIC_ID);
+
+      const options = {
+        zoompan : 'string'
+      }
+
+      plugin({
+        cldAsset: cldImage,
+        options
+      });
+
+      expect(cldImage.toURL()).toContain(`e_zoompan:${options.zoompan}`);
+    });
+
+    it('should add a custom options', () => {
+
+      const cldImage = cld.image(TEST_PUBLIC_ID);
+
+      const options = {
+        zoompan : {
+          options: "string",
+          loop: true
+        }
+      }
+
+      plugin({
+        cldAsset: cldImage,
+        options
+      });
+
+      expect(cldImage.toURL()).toContain(`e_zoompan${options.zoompan.options}`);
+      expect(cldImage.toURL()).toContain('e_loop');
+    });
+
+    it('should add a custom loop option', () => {
+
+      const cldImage = cld.image(TEST_PUBLIC_ID);
+
+      const options = {
+        zoompan : {
+          options: "string",
+          loop: "string"
+        }
+      }
+
+      plugin({
+        cldAsset: cldImage,
+        options
+      });
+
+      expect(cldImage.toURL()).toContain(`e_loop${options.zoompan.loop}`);
+    });
+
+    it('should not override format', () => {
+
+      const cldImage = cld.image(TEST_PUBLIC_ID);
+
+      const options = {
+        zoompan : false
+      }
+
+      const result = plugin({
+                      cldAsset: cldImage,
+                      options
+                    });
+
+      expect(result.options.format).toBe(undefined);
+    });
+
+  });
+});
