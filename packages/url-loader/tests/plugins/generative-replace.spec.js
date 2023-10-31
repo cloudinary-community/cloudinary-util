1968e8544069bbfd99291fa604be89e624433ffc
Michael Jolley <mike@sparcapp.io>
2023-10-31 13:36:33 -0500

    feat: Generative Replace (#68)

---
+++ packages/url-loader/tests/plugins/generative-replace.spec.js
@@ -0,0 +1,99 @@
+import { Cloudinary } from "@cloudinary/url-gen";
+
+import * as generativeReplacePlugin from "../../src/plugins/generative-replace";
+
+const { plugin } = generativeReplacePlugin;
+
+const cld = new Cloudinary({
+  cloud: {
+    cloudName: "test-cloud-name",
+  },
+});
+
+const TEST_PUBLIC_ID = "test-public-id";
+
+describe("Plugins", () => {
+  describe("Generative Replace", () => {
+    it("should replace with object", () => {
+      const cldImage = cld.image(TEST_PUBLIC_ID);
+
+      const options = {
+        replace: {
+          from: "apple",
+          to: "orange",
+        },
+      };
+
+      plugin({
+        cldAsset: cldImage,
+        options,
+      });
+
+      expect(cldImage.toURL()).toContain(`e_gen_replace:from_apple;to_orange`);
+    });
+
+    it("should replace with object and preserved geometry", () => {
+      const cldImage = cld.image(TEST_PUBLIC_ID);
+
+      const options = {
+        replace: {
+          from: "apple",
+          to: "orange",
+          preserveGeometry: true,
+        },
+      };
+
+      plugin({
+        cldAsset: cldImage,
+        options,
+      });
+
+      expect(cldImage.toURL()).toContain(
+        `e_gen_replace:from_apple;to_orange;preserve-geometry_true`
+      );
+    });
+
+    it("should replace with array", () => {
+      const cldImage = cld.image(TEST_PUBLIC_ID);
+
+      const options = {
+        replace: ["apple", "orange"],
+      };
+
+      plugin({
+        cldAsset: cldImage,
+        options,
+      });
+
+      expect(cldImage.toURL()).toContain(`e_gen_replace:from_apple;to_orange`);
+    });
+
+    it("should replace with array and preserved geometry", () => {
+      const cldImage = cld.image(TEST_PUBLIC_ID);
+
+      const options = {
+        replace: ["apple", "candy bar", "true"],
+      };
+
+      plugin({
+        cldAsset: cldImage,
+        options,
+      });
+
+      expect(cldImage.toURL()).toContain(
+        `e_gen_replace:from_apple;to_candy%20bar;preserve-geometry_true`
+      );
+    });
+
+    it("should not attempt generative replace", () => {
+      const cldImage = cld.image(TEST_PUBLIC_ID);
+
+      plugin({
+        cldAsset: cldImage,
+        options: {},
+      });
+
+      expect(cldImage.toURL()).not.toContain(`e_gen_replace`);
+    });
+  });
+});
