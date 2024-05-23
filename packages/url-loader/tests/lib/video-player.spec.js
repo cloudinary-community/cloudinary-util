da6b56d591efaf767669be722c0c954720bb9463
Colby Fayock <colbyfayock@users.noreply.github.com>
2024-05-22 21:45:30 -0400

    feat: getVideoPlayerOptions (#156)
    # Description
    
    This lifts the player options logic from Next Cloudinary, which should
    basically reflect Nuxt and Svelte.
    
    This allows us to maintain a single set of configuration options for the
    player that will support each framework that creates a similar
    CldVideoPlayer component.
    
    It would work as such:
    ```
    const playerOptions = getVideoPlayerOptions(props, config);
    videoPlayer(videoRef.current, playerOptions);
    ```
    
    You can see some examples inside of the tests.
    
    As of now, I left out `className` and `id` as returned options as the
    function's intent is to return "player options", which would be invalid
    values for the player. This in come cases could cause unintentional side
    effects, so opting to not return those.
    
    A potential solution would be to return an object, with player options
    as a property, but i'm leaning towards it being an unintuitive return
    shape given the name of the function. Open to thoughts on this.
    
    As far as where this module is located, I opted to include it in
    `/url-loader` with the idea being that the family of component support
    functions could live under that package and in a future major release,
    we could rename it to something more appropriate for all. I'm open to
    other ideas / suggestions on this.
    
    ## Issue Ticket Number
    
    Fixes #155 
    
    <!-- Specify above which issue this fixes by referencing the issue
    number (`#<ISSUE_NUMBER>`) or issue URL. -->
    <!-- Example: Fixes
    https://github.com/colbyfayock/cloudinary-util/issues/<ISSUE_NUMBER> -->
    
    ## Type of change
    
    <!-- Please select all options that are applicable. -->
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected)
    - [ ] Fix or improve the documentation
    - [ ] This change requires a documentation update
    
    
    # Checklist
    
    <!-- These must all be followed and checked. -->
    
    - [ ] I have followed the contributing guidelines of this project as
    mentioned in [CONTRIBUTING.md](/CONTRIBUTING.md)
    - [ ] I have created an
    [issue](https://github.com/colbyfayock/cloudinary-util/issues) ticket
    for this PR
    - [ ] I have checked to ensure there aren't other open [Pull
    Requests](https://github.com/colbyfayock/cloudinary-util/pulls) for the
    same update/change?
    - [ ] I have performed a self-review of my own code
    - [ ] I have run tests locally to ensure they all pass
    - [ ] I have commented my code, particularly in hard-to-understand areas
    - [ ] I have made corresponding changes needed to the documentation

---
+++ packages/url-loader/tests/lib/video-player.spec.js
@@ -0,0 +1,193 @@
+import { describe, it, expect } from 'vitest';
+
+import { getVideoPlayerOptions } from '../../src/lib/video-player';
+
+describe('video-player', () => {
+  describe('getVideoPlayerOptions', () => {
+    describe('Basic', () => {
+      it('should return create an options object with minimal config', () => {
+        const options = {
+          width: '1620',
+          height: '1080',
+          src: 'videos/mountain-stars',
+        }
+        
+        const config = {
+          cloud: {
+            cloudName: 'testcloud'
+          }
+        };
+  
+        const expectedOptions = {
+          aspectRatio: '1620:1080',
+          autoplay: false,
+          autoplayMode: undefined,
+          cloud_name: 'testcloud',
+          controls: true,
+          height: '1080',
+          language: undefined,
+          languages: undefined,
+          loop: false,
+          muted: false,
+          privateCdn: undefined,
+          publicId: 'videos/mountain-stars',
+          secureDistribution: undefined,
+          showLogo: true,
+          transformation: [
+            { quality: 'auto' },
+            undefined
+          ],
+          width: '1620',
+        }
+  
+        expect(getVideoPlayerOptions(options, config)).toMatchObject(expectedOptions)
+      });
+
+      it('should configure custom quality', () => {
+        const options = {
+          width: '1620',
+          height: '1080',
+          src: 'videos/mountain-stars',
+          quality: 50,
+        }
+        
+        const config = {
+          cloud: {
+            cloudName: 'testcloud'
+          }
+        };
+  
+        expect(getVideoPlayerOptions(options, config)).toMatchObject({
+          transformation: [
+            { quality: options.quality },
+            undefined
+          ],
+        })
+      });
+    })
+    
+    describe('Playback', () => {
+      it('should configure ABR via source types', () => {
+        const options = {
+          width: '1620',
+          height: '1080',
+          src: 'videos/mountain-stars',
+          transformation: {
+            streaming_profile: 'hd',
+          },
+          sourceTypes: ['hls'],
+        }
+        
+        const config = {
+          cloud: {
+            cloudName: 'testcloud'
+          }
+        };
+    
+        expect(getVideoPlayerOptions(options, config)).toMatchObject({
+          transformation: [
+            { quality: 'auto' },
+            { streaming_profile: 'hd' }
+          ],
+          sourceTypes: ['hls'],
+        });
+      });
+    })
+
+    describe('Customization', () => {
+      it('should configure custom logo and colors', () => {
+        const options = {
+          width: '1620',
+          height: '1080',
+          src: 'videos/mountain-stars',
+          fontFace: 'Colby',
+          logo: {
+            imageUrl: 'https://image.com',
+            onClickUrl: 'https://spacejelly.dev'
+          },
+          colors: {
+            accent: '#ff0000',
+            base: '#00ff00',
+            text: '#0000ff'
+          }
+        }
+        
+        const config = {
+          cloud: {
+            cloudName: 'testcloud'
+          }
+        };
+
+        expect(getVideoPlayerOptions(options, config)).toMatchObject({
+          fontFace: options.fontFace,
+          logoImageUrl: options.logo.imageUrl,
+          logoOnclickUrl: options.logo.onClickUrl,
+          showLogo: true,
+          colors: options.colors
+        });
+      });
+
+      it('should set a custom poster using a string', () => {
+        const options = {
+          width: '1620',
+          height: '1080',
+          src: 'videos/mountain-stars',
+          poster: 'string'
+        }
+        
+        const config = {
+          cloud: {
+            cloudName: 'testcloud'
+          }
+        };
+
+        const playerOptions = getVideoPlayerOptions(options, config);
+
+        expect(playerOptions.posterOptions.publicId).toContain(options.poster)
+      });
+
+      it('should set a custom poster using constructCloudinaryUrl options syntax with public ID inherited', () => {
+        const options = {
+          width: '1620',
+          height: '1080',
+          src: 'videos/mountain-stars',
+          poster: {
+            tint: 'equalize:80:blue:blueviolet'
+          }
+        }
+        
+        const config = {
+          cloud: {
+            cloudName: 'testcloud'
+          }
+        };
+
+        const playerOptions = getVideoPlayerOptions(options, config);
+
+        expect(playerOptions.posterOptions.publicId).toContain(`https://res.cloudinary.com/${config.cloud.cloudName}/video/upload/e_tint:${options.poster.tint}/f_auto:image/q_auto/v1/${options.src}`);
+      });
+
+      it('should set a custom poster using constructCloudinaryUrl options syntax with custom thumb public ID', () => {
+        const options = {
+          width: '1620',
+          height: '1080',
+          src: 'videos/mountain-stars',
+          poster: {
+            src: 'my-custom-public-id',
+            tint: 'equalize:80:blue:blueviolet'
+          }
+        }
+        
+        const config = {
+          cloud: {
+            cloudName: 'testcloud'
+          }
+        };
+
+        const playerOptions = getVideoPlayerOptions(options, config);
+
+        expect(playerOptions.posterOptions.publicId).toContain(`https://res.cloudinary.com/${config.cloud.cloudName}/image/upload/e_tint:${options.poster.tint}/f_auto/q_auto/${options.poster.src}`);
+      });
+    })
+  });
+});
