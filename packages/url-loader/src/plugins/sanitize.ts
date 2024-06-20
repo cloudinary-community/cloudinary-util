bae82140215179f0d7091f440aa5530d25daf17d
David Blass <david@arktype.io>
2024-06-20 12:57:39 -0400

    initial plugin refactor

--- packages/url-loader/src/plugins/sanitize.ts
+++ packages/url-loader/src/plugins/sanitize.ts
@@ -1,30 +1,35 @@
-import { z } from 'zod';
-
-import { ImageOptions } from '../types/image';
-import { PluginSettings } from '../types/plugins';
+import { z } from "zod";
+import { ImageOptions } from "../types/image";
+import { type TransformationPlugin } from "../types/plugins";
 
 export const props = {
-  sanitize: z.boolean()
-    .describe(JSON.stringify({
-      text: 'Runs a sanitizer on SVG images.',
-      url: 'https://cloudinary.com/documentation/transformation_reference#fl_sanitize'
-    }))
+  sanitize: z
+    .boolean()
+    .describe(
+      JSON.stringify({
+        text: "Runs a sanitizer on SVG images.",
+        url: "https://cloudinary.com/documentation/transformation_reference#fl_sanitize",
+      })
+    )
     .optional(),
 };
 
-export const assetTypes = ['image', 'images'];
+export const sanitizeProps = {};
 
-export function plugin(props: PluginSettings<ImageOptions>) {
-  const { cldAsset, options } = props;
-  const { sanitize = true } = options;
+export const sanitizePlugin = {
+  props: sanitizeProps,
+  assetTypes: ["image", "images"],
+  plugin: ({ cldAsset, options }) => {
+    const { sanitize = true } = options;
 
-  const shouldApplySanitizer: boolean =
-    sanitize &&
-    (options.format === 'svg' || cldAsset.publicID.endsWith('.svg'));
+    const shouldApplySanitizer: boolean =
+      sanitize &&
+      (options.format === "svg" || cldAsset.publicID.endsWith(".svg"));
 
-  if (shouldApplySanitizer) {
-    cldAsset.effect('fl_sanitize');
-  }
+    if (shouldApplySanitizer) {
+      cldAsset.effect("fl_sanitize");
+    }
 
-  return {};
-}
+    return {};
+  },
+} satisfies TransformationPlugin<ImageOptions>;
--- packages/url-loader/src/types/image.ts
+++ packages/url-loader/src/types/image.ts
@@ -1,27 +1,29 @@
-import { z } from 'zod';
+import { z } from "zod";
+import { assetOptionsSchema } from "./asset";
+import { defaultImageProps } from "../plugins/default-image";
+import { enhanceProps } from "../plugins/enhance";
+import { fillBackgroundProps } from "../plugins/fill-background";
+import { recolorProps } from "../plugins/recolor";
+import { removeProps } from "../plugins/remove";
+import { replaceProps } from "../plugins/replace";
+import { restoreProps } from "../plugins/restore";
+import { zoompanProps } from "../plugins/zoompan";
 
-import { assetOptionsSchema } from './asset';
+export const imageOptionsSchema = assetOptionsSchema.merge(
+  z.object({
+    // Spreading plugins instead of extend or merge to avoid excessive schema warning
+    // https://github.com/microsoft/TypeScript/issues/34933#issuecomment-1772787785
+    ...defaultImageProps,
+    ...enhanceProps,
+    ...fillBackgroundProps,
+    ...recolorProps,
+    ...removeProps,
+    ...replaceProps,
+    ...restoreProps,
+    ...zoompanProps,
+  })
+);
 
-import { props as defaultImagePluginProps } from '../plugins/default-image';
-import { props as fillBackgroundPluginProps } from '../plugins/fill-background';
-import { props as recolorPluginProps } from '../plugins/recolor';
-import { props as removePluginProps } from '../plugins/remove';
-import { props as enhancePluginProps } from '../plugins/enhance';
-import { props as restorePluginProps } from '../plugins/restore';
-import { props as replacePluginProps } from '../plugins/replace';
-import { props as zoompanPluginProps } from '../plugins/zoompan';
+type _ImageOptions = z.infer<typeof imageOptionsSchema>;
 
-export const imageOptionsSchema = assetOptionsSchema.merge(z.object({
-  // Spreading plugins instead of extend or merge to avoid excessive schema warning
-  // https://github.com/microsoft/TypeScript/issues/34933#issuecomment-1772787785
-  ...defaultImagePluginProps,
-  ...enhancePluginProps,
-  ...fillBackgroundPluginProps,
-  ...recolorPluginProps,
-  ...removePluginProps,
-  ...replacePluginProps,
-  ...restorePluginProps,
-  ...zoompanPluginProps,
-}))
-
-export type ImageOptions = z.infer<typeof imageOptionsSchema>;
+export interface ImageOptions extends _ImageOptions {}
