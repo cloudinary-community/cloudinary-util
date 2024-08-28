8b7a1c7b0a0bae9b32c8fbd8ac53960caf340c94
Willow (GHOST) <ghostdevbusiness@gmail.com>
2024-08-28 02:15:14 +0100

    feat: add pollForProcessingImage (#187)
    # Description
    
    <!-- Include a summary of the change made and also list the dependencies
    that are required if any -->
    
    ## Type of change
    
    <!-- Please select all options that are applicable. -->
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [x] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected)
    - [ ] Fix or improve the documentation
    - [ ] This change requires a documentation update
    
    # Checklist
    
    <!-- These must all be followed and checked. -->
    
    - [x] I have followed the contributing guidelines of this project as
    mentioned in [CONTRIBUTING.md](/CONTRIBUTING.md)
    - [ ] I have created an
    [issue](https://github.com/colbyfayock/cloudinary-util/issues) ticket
    for this PR
    - [x] I have checked to ensure there aren't other open [Pull
    Requests](https://github.com/colbyfayock/cloudinary-util/pulls) for the
    same update/change?
    - [x] I have performed a self-review of my own code
    - [x] I have run tests locally to ensure they all pass
    - [x] I have commented my code, particularly in hard-to-understand areas
    - [x] I have made corresponding changes needed to the documentation

--- packages/util/src/index.ts
+++ packages/util/src/index.ts
@@ -3,6 +3,8 @@ export {
   getPublicId,
   getTransformations,
   parseUrl,
+  pollForProcessingImage,
+  type PollForProcessingImageOptions,
 } from "./lib/cloudinary.js";
 export type { ParseUrl } from "./lib/cloudinary.js";
 export { convertColorHexToRgb, testColorIsHex } from "./lib/colors.js";
